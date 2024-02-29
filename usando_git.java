61ded09469db2916b638866a0bc26bff31fa5351
hugomttn <hugomartinn18@gmail.com>
2024-02-29 15:51:01 +0100

    commit del .gitignore

--- usando_git.java
+++ usando_git.java
@@ -1,7 +1,7 @@
 public class usando_git{
 	public static void main(String[] args){
 		System.out.println("Proyecto con GIT");
-		System.out.println("Modificando a la clase");
+		System.out.println("Modificando a la clase2");
 	}
 }
 
